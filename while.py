6718dff32994e60f35cee3f534d44b12376eb16c
Navrozbek01 <maqsadbekqoziyev245@gmail.com>
2025-12-01 17:47:04 +0500

    while
    masala bor!

---
+++ while.py
@@ -0,0 +1,29 @@
+# x = input("Parolni kiriting:")
+# while x != "qwerty123":
+#     print("parol notog'ri,qayta urinib ko'ring.")
+#     x=input("parolni kiriting: ")
+#     if x == "qwerty123":
+#         print("Parol to'gri,xush kelibsiz!")
+#         break
+
+
+# a = int(input("sonni kiriting: "))
+# yigindi= 0
+#
+# while a >0:
+#     raqam = a % 10
+#     yigindi +=raqam
+#     a = a // 10
+#     print("Raqamlar yigindisi",yigindi)
+
+
+# son = int(input("Sonni kiriting: "))
+#
+# yigindi = 0
+# asl_son = son
+#
+# while asl_son > 0:
+#     raqam = asl_son % 10
+#     yigindi = yigindi + raqam
+#     asl_son = asl_son // 10
+#     print(f"{son} ning raqamlari yigindisi: {yigindi}")
